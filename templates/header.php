e8fe97cff84a72bc3f02e9a3cc6ba9322d227d0c
Grasiela Texeira <grasy.mst@gmail.com>
2022-07-08 15:15:47 -0300

    Alterado o header

--- templates/header.php
+++ templates/header.php
@@ -17,3 +17,16 @@ include_once("config/url.php");
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css" integrity="sha512-KfkfwYDsLkIlwQp6LFnl8zNdLGxu9YAA1QvwINks4PhcElQSvqcyVLLD9aMhXd13uQjoXtEKNosOWaZqXgel0g==" crossorigin="anonymous" referrerpolicy="no-referrer" />                 
     <!-- Css -->
     <link rel="stylesheet" href="<?= $BASE_URL ?>css/styles.css">
+</head>
+<body>
+    <header>
+        <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
+            <a class="navbar-brand" href="<?= $BASE_URL ?> index.php">
+                <img src="<?= $BASE_URL ?>img/logo.svg" alt="Agenda">
+            </a>   
+            <div>
+                <div class="navbar-nav">    
+                    <a class="nav-link active" id="home-link" href="<?= $BASE_URL ?> index.php">Agenda</a>
+                    <a class="nav-link active" id="home-link" href="<?= $BASE_URL ?> create.php">Adicionar contato</a>
+        </nav>
+    </header>
