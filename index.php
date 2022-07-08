83a4bda52acea0e73b56316f91e68abd3af9b559
Grasiela Texeira <grasy.mst@gmail.com>
2022-07-08 15:15:04 -0300

    Alterado css

--- index.php
+++ index.php
@@ -1,4 +1,16 @@
-    <h1>Testando agenda</h1>
+<?php
+
+include_once("templates/header.php");
+
+?>
+   
+   <h1>Testando agenda</h1>
     <i class="fas fa-eye"></i>
 </body>
 </html>
+
+<?php
+
+include_once("templates/footer.php");
+
+?>
