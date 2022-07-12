758590dec000ea1e3cbd1258b86af49cbb35c712
Grasiela Texeira <grasy.mst@gmail.com>
2022-07-12 17:42:03 -0300

    Editando contato

--- edit.php
+++ edit.php
@@ -8,8 +8,8 @@
       <input type="hidden" name="type" value="edit">
       <input type="hidden" name="id" value="<?= $contact['id'] ?>">
       <div class="form-group">
-        <label for="name">Nome do contato:</label>
-        <input type="text" class="form-control" id="name" name="name" placeholder="Digite o nome" value="<?= $contact['nome'] ?>" required>
+        <label for="nome">Nome do contato:</label>
+        <input type="text" class="form-control" id="nome" name="nome" placeholder="Digite o nome" value="<?= $contact['nome'] ?>" required>
       </div>
       <div class="form-group">
         <label for="phone">Telefone do contato:</label>
